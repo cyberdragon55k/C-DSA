bb43316ac790fa4e6e85bcb8eab9707022c6fb3c
Cyberdragon <aditya5namdeo@gmail.com>
2025-11-25 09:51:31 +0530

    late to push

---
+++ d2.cpp
@@ -0,0 +1,22 @@
+/* #include<iostream>
+using namespace std;
+int main(){
+    int i =1;
+    while (i<=10){
+        cout<<i<<" ";
+        i+=1;
+
+    }
+}
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int num =8;
+    int i = 1;
+    while(i<=10){
+        cout << num*i<<"  ";
+        i++;
+
+    }
+}
